0f3598072e7c901d0ca120f6fdeacca27a98698d
Nathan McClora <nathan.mcclora@gmail.com>
2026-06-19 14:10:01 -0400

    Same Same

---
+++ Python Code/PythonIntro/datatypes.py
@@ -0,0 +1,23 @@
+my_cool_variable = "Hello, World!"
+print(my_cool_variable)
+print("  " )
+print("  " )
+print("  " )
+print("  " )
+print("  " )
+print("  " )
+print("  " )
+
+
+my_cool_variable2 = 44
+print(my_cool_variable2)
+print("  " )
+print("  " )
+print("  " )
+print("  " )
+print("  " )
+print("  " )
+
+    
+my_cool_variable3 = 3.14
+print(my_cool_variable3)
