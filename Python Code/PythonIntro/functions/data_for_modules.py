3d4b4590fb028e103ad0899193f94300bb6e1639
Nathan McClora <nathan.mcclora@gmail.com>
2026-07-02 05:28:37 -0400

    Changed the name of folder. changing names of files in the process.

---
+++ Python Code/PythonIntro/functions/data_for_modules.py
@@ -0,0 +1,4 @@
+from my_modules import cube_number, greet_person
+
+print(cube_number(3))  # This will print the cube of 3, which is 27
+print(greet_person ("Alice"))  # This will print a greeting for Alice, which is "Hello, Alice!
