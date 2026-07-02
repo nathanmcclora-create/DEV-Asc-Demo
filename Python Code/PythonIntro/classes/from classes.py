51b7685ce244a295a9c0f0d6b30f529ff9873a55
Nathan McClora <nathan.mcclora@gmail.com>
2026-07-02 05:25:42 -0400

    This format class I created did not work. Must figure out my?

---
+++ Python Code/PythonIntro/classes/from classes.py
@@ -0,0 +1,5 @@
+from classes.my_classes import Car
+
+my_car = Car(make="Toyota", model="Camry", year=2020, mileage=15000, condition="Good", color="Blue")
+
+my_wife_car = Car(make="Honda", model="Civic", year=2018, mileage=30000, condition="Excellent", color="Red")
--- Python Code/PythonIntro/classes/my_classes.py
+++ Python Code/PythonIntro/classes/my_classes.py
@@ -11,3 +11,11 @@ class Car:
         self.mileage = mileage
         self.condition = condition
         self.color = color
+
+
+
+my_car = Car(make="Toyota", model="Camry", year=2020, mileage=15000, condition="Good", color="Blue")
+
+my_wife_car = Car(make="Honda", model="Civic", year=2018, mileage=30000, condition="Excellent", color="Red")
+
+from classes.my_classes import Car
